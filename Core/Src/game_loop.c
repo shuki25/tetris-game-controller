ab1f6de4d280e7723b56dafc1b576cb8c8c00e6b
Yoel Baer Buzgalo <ybb5956@g.rit.edu>
2025-01-10 21:19:49 -0500

    Update game loop for test

--- Core/Src/game_loop.c
+++ Core/Src/game_loop.c
@@ -19,25 +19,39 @@
  ******************************************************************************
  */
 
+#include <stdint.h>
+
+#include "renderer.h"
 #include "main.h"
-#include "stdint.h"
 
 #include "snes_controller.h"
 #include "game_loop.h"
 #include "ssd1306.h"
 #include "ssd1306_fonts.h"
 #include "ws2812.h"
-#include "matrix_rendering.h"
+#include "matrix.h"
 #include "itm_debug.h"
 #include "util.h"
 #include "cmsis_os.h"
 
+// Debugger Expression Variables
+uint32_t game_loop_counter = 0;
+uint32_t render_count = 0;
+
+// Matrix Variables
+matrix_t matrix;
+
+// Render Variables
 uint8_t update_screen_flag;
 led_t led;
 uint8_t *brightness_lookup = NULL;
-volatile uint32_t game_loop_counter = 0;
-volatile uint32_t render_count = 0;
+uint32_t render_delay = (1000000 / 10);
+renderer_t rendering_info;
+
+// Controller Variables
 snes_controller_t snes_controller;
+
+// TIM Variables
 extern TIM_HandleTypeDef htim3;
 
 /**
@@ -71,8 +85,8 @@ void game_init() {
  */
 void game_loop(void) {
     snes_controller_status_t controller_status;
-    matrix_rendering_status_t matrix_status;
-    led_matrix_t matrix;
+    matrix_status_t matrix_status;
+    renderer_status_t rendering_status;
 
     char output_buffer[80];
 
@@ -84,11 +98,11 @@ void game_loop(void) {
 
     // TODO: Initialize game components
     /* Generate and initialize the brightness lookup table */
-    brightness_lookup = generate_brightness_lookup_table(10);
 
-    matrix_status = matrix_rendering_init(&matrix, &led, &htim3, TIM_CHANNEL_1, 1000000 / 10, 512);
-    controller_status = snes_controller_init(&snes_controller, SNES_LATCH_GPIO_Port, SNES_LATCH_Pin,
-    SNES_CLOCK_GPIO_Port, SNES_CLOCK_Pin, SNES_DATA0_GPIO_Port, SNES_DATA0_Pin, 60);
+    controller_status = snes_controller_init(&snes_controller,
+    SNES_LATCH_GPIO_Port, SNES_LATCH_Pin,
+    SNES_CLOCK_GPIO_Port, SNES_CLOCK_Pin, SNES_DATA0_GPIO_Port,
+    SNES_DATA0_Pin, 60);
     if (controller_status != SNES_CONTROLLER_OK) {
 #if DEBUG_OUTPUT
         printf("SNES controller initialization failed\n");
@@ -96,6 +110,30 @@ void game_loop(void) {
     }
 
     // TODO: Start the main game loop
+    brightness_lookup = generate_brightness_lookup_table(10);
+
+    if (brightness_lookup != NULL) {
+#if DEBUG_OUTPUT
+        printf("Generated brightness lookup table\n");
+#endif
+    }
+
+    matrix_status = matrix_init(&matrix);
+
+    if (matrix_status == MATRIX_OK) {
+#if DEBUG_OUTPUT
+        printf("Matrix initialization success\n");
+#endif
+    }
+
+    rendering_status = renderer_init(&rendering_info, &matrix, &led,
+            &htim3, TIM_CHANNEL_1, render_delay);
+
+    if (rendering_status == RENDERER_OK) {
+#if DEBUG_OUTPUT
+        printf("Rendering initialization success\n");
+#endif
+    }
 
     for (;;) {
         // TODO: Respond to scoreboard requests
@@ -130,9 +168,10 @@ void game_loop(void) {
         // TODO: Get the next tetrimino from the RNG
 
         // TODO: Render matrix and update LED grid
-        matrix_status = matrix_rendering_test_render(&matrix);
+
+        rendering_status = renderer_test_render(&rendering_info);
 #if DEBUG_OUTPUT
-        if (matrix_status == MATRIX_RENDERING_UPDATED) {
+        if (rendering_status == RENDERER_UPDATED) {
             render_count++;
         }
 #endif
