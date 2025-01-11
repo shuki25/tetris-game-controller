154403f42152780fd1741df4f32299c8cf47d388
dr. josh butler <31372342+shuki25@users.noreply.github.com>
2025-01-11 12:47:36 -0500

    add timer expired check functions

--- Core/Src/util.c
+++ Core/Src/util.c
@@ -23,6 +23,54 @@
 #include "main.h"
 #include "util.h"
 
+/**
+ * @brief  Check if time has expired (rollover is handled)
+ * @param  start: start time
+ * @param  delay: delay time
+ * @retval 1 if expired, 0 if not expired
+ */
+uint8_t util_time_expired_delay(uint32_t start, uint32_t delay) {
+    uint32_t end = start + delay;
+    uint32_t rollover = 0xFFFFFFFF;
+
+    if (start > end)
+        end = rollover - start + end; // It will rollover, so we need to adjust the end time
+
+    if (TIM2->CNT >= end && start < end) { // Normal case
+        return 1;
+    } else if (TIM2->CNT < start && TIM2->CNT >= end) { // Rollover case
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief  Check if time has expired (rollover is handled)
+ * @param  start: start time
+ * @param  end: end time
+ * @retval 1 if expired, 0 if not expired
+ */
+uint8_t util_time_expired(uint32_t start, uint32_t end) {
+    uint32_t rollover = 0xFFFFFFFF;
+
+    if (start > end)
+        end = rollover - start + end; // It will rollover, so we need to adjust the end time
+
+    if (TIM2->CNT >= end && start < end) { // Normal case
+        return 1;
+    } else if (TIM2->CNT < start && TIM2->CNT >= end) { // Rollover case
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * @brief  Delay in microseconds
+ * @param  us: microseconds
+ * @retval None
+ */
 void util_delay_us(uint32_t us) {
     uint32_t start = TIM2->CNT;
     uint32_t end = start + us;
@@ -41,6 +89,12 @@ void util_delay_us(uint32_t us) {
     }
 }
 
+/**
+ * @brief  Convert 32-bit number to binary string
+ * @param  num: 32-bit number
+ * @param  binary: binary string
+ * @retval None
+ */
 void util_to_binary32(uint32_t num, char *binary) {
     for (int i = 0; i < 32; i++) {
         binary[i] = (num & (1 << (31 - i))) ? '1' : '0';
@@ -48,6 +102,12 @@ void util_to_binary32(uint32_t num, char *binary) {
     binary[32] = '\0';
 }
 
+/**
+ * @brief  Convert 16-bit number to binary string
+ * @param  num: 16-bit number
+ * @param  binary: binary string
+ * @retval None
+ */
 void util_to_binary16(uint16_t num, char *binary) {
     for (int i = 0; i < 16; i++) {
         binary[i] = (num & (1 << (15 - i))) ? '1' : '0';
@@ -55,6 +115,12 @@ void util_to_binary16(uint16_t num, char *binary) {
     binary[16] = '\0';
 }
 
+/**
+ * @brief  Convert 8-bit number to binary string
+ * @param  num: 8-bit number
+ * @param  binary: binary string
+ * @retval None
+ */
 void util_to_binary8(uint8_t num, char *binary) {
     for (int i = 0; i < 8; i++) {
         binary[i] = (num & (1 << (7 - i))) ? '1' : '0';
