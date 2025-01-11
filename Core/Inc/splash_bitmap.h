ba152d56aea4812d72ca78302aebb4589677e1ed
dr. josh butler <31372342+shuki25@users.noreply.github.com>
2025-01-10 22:36:48 -0500

    add splash bitmap

---
+++ Core/Inc/splash_bitmap.h
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  * @file           : splash_bitmap.h
+  * @author         : Dr. Joshua Butler
+  * @date           : Dec 9, 2024
+  * @brief          : Tetrimino representation for Classic Tetris on LED Grid
+  ******************************************************************************
+  * @attention
+  *
+  * 2025 Imagine RIT Project: Classic Tetris on LED Grid
+  *
+  * Copyright (c) 2024-25 Rochester Institute of Technology.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+#ifndef INC_SPLASH_BITMAP_H_
+#define INC_SPLASH_BITMAP_H_
+
+extern const unsigned char *tetrimino_allArray[];
+
+
+#endif /* INC_SPLASH_BITMAP_H_ */
