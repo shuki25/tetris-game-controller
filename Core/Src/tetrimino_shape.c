78d5fcb36b5af798da5fcced1a264d4c575f6144
DE4Fxx <65514584+DE4Fxx@users.noreply.github.com>
2025-01-18 11:43:04 -0500

    Patrick tetromino shapes (#122)
    * Added tetromino shapes
    * updated comments on tetrimino shapes
    * updated header comment
    * added offset positions in comments for easy tracking
    ---------
    Co-authored-by: dr. josh butler <31372342+shuki25@users.noreply.github.com>

--- Core/Src/tetrimino_shape.c
+++ Core/Src/tetrimino_shape.c
@@ -1,8 +1,22 @@
-/*
- * tetrimino_shape.c
+/**
+ ******************************************************************************
+ * @file           : game_loop.c
+ * @author         : Patrick Lebeau Jr., Dr. Joshua Butler
+ * @date           : Jan 9, 2024
+ * @brief          : Tetrimino shape definitions
+ ******************************************************************************
+ * @attention
  *
- *  Created on: Jan 9, 2025
- *      Author: jdbnts
+ * 2025 Imagine RIT Project: Classic Tetris on LED Grid
+ *
+ * Copyright (c) 2024-25 Rochester Institute of Technology.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
  */
 
 #include "tetrimino_shape.h"
@@ -24,25 +38,25 @@
  */
 
 const uint8_t tetrimino_shape[] = {
-    0x00, 0x04, 0x0e, 0x00, 0x00, // T - up
-    0x00, 0x04, 0x06, 0x04, 0x00, // T - right
-    0x00, 0x00, 0x0e, 0x04, 0x00, // T - down (spawn)
-    0x00, 0x04, 0x0c, 0x04, 0x00, // T - left
-    0x00, 0x00, 0x00, 0x00, 0x00, // J - up
-    0x00, 0x00, 0x00, 0x00, 0x00, // J - right
-    0x00, 0x00, 0x00, 0x00, 0x00, // J - down (spawn)
-    0x00, 0x00, 0x00, 0x00, 0x00, // J - left
-    0x00, 0x00, 0x00, 0x00, 0x00, // Z - horizontal (spawn)
-    0x00, 0x00, 0x00, 0x00, 0x00, // Z - vertical
-    0x00, 0x00, 0x00, 0x00, 0x00, // O - spawn
-    0x00, 0x00, 0x00, 0x00, 0x00, // S - horizontal (spawn)
-    0x00, 0x00, 0x00, 0x00, 0x00, // S - vertical
-    0x00, 0x00, 0x00, 0x00, 0x00, // L - up
-    0x00, 0x00, 0x00, 0x00, 0x00, // L - right
-    0x00, 0x00, 0x00, 0x00, 0x00, // L - down (spawn)
-    0x00, 0x00, 0x00, 0x00, 0x00, // L - left
-    0x00, 0x00, 0x00, 0x00, 0x00, // I - vertical
-    0x00, 0x00, 0x00, 0x00, 0x00, // I - horizontal (spawn)
+    0x00, 0x04, 0x0e, 0x00, 0x00, // T - up [offset 0]
+    0x00, 0x04, 0x06, 0x04, 0x00, // T - right [offset 5]
+    0x00, 0x00, 0x0e, 0x04, 0x00, // T - down (spawn) [offset 10]
+    0x00, 0x04, 0x0c, 0x04, 0x00, // T - left [offset 15]
+    0x00, 0x08, 0x0e, 0x00, 0x00, // J - up [offset 20]
+    0x00, 0x06, 0x04, 0x04, 0x00, // J - right [offset 25]
+    0x00, 0x00, 0x0e, 0x02, 0x00, // J - down (spawn) [offset 30]
+    0x00, 0x04, 0x04, 0x0c, 0x00, // J - left [offset 35]
+    0x00, 0x00, 0x0c, 0x06, 0x00, // Z - horizontal (spawn) [offset 40]
+    0x00, 0x02, 0x06, 0x04, 0x00, // Z - vertical [offset 45]
+    0x00, 0x00, 0x0c, 0x0c, 0x00, // O - spawn [offset 50]
+    0x00, 0x00, 0x06, 0x0c, 0x00, // S - horizontal (spawn) [offset 55]
+    0x00, 0x02, 0x06, 0x04, 0x00, // S - vertical [offset 60]
+    0x00, 0x02, 0x0e, 0x00, 0x00, // L - up [offset 65]
+    0x00, 0x04, 0x04, 0x06, 0x00, // L - right [offset 70]
+    0x00, 0x00, 0x0e, 0x08, 0x00, // L - down (spawn) [offset 75]
+    0x00, 0x0c, 0x04, 0x04, 0x00, // L - left [offset 80]
+    0x04, 0x04, 0x04, 0x04, 0x00, // I - vertical [offset 85]
+    0x00, 0x00, 0x1e, 0x00, 0x00, // I - horizontal (spawn) [offset 90]
 };
 
 /**
@@ -50,6 +64,8 @@ const uint8_t tetrimino_shape[] = {
  *
  * For example, the T tetrimino has four rotations, so the offsets are 0, 5, 15, and 20.
  * The offsets are used to reference the tetrimino_shape array to get the shape for each rotation.
+ *
+ * Offset mapping is ordered by tetrimino piece (T, J, Z, O, S, L, I) and then by rotation position (up, right, down, left).
  *
  */
 
@@ -60,7 +76,7 @@ const uint8_t tetrimino_shape_offset_lut[TETRIMINO_COUNT][TETRIMINO_ROTATION_COU
    { 50, 50, 50, 50},   // O
    { 55, 60, 55, 60},   // S
    { 65, 70, 75, 80},   // L
-   { 85, 85, 85, 85}    // I
+   { 85, 90, 85, 90}    // I
 };
 
 /**
@@ -74,5 +90,4 @@ const uint8_t tetrimino_shape_offset_lut[TETRIMINO_COUNT][TETRIMINO_ROTATION_COU
 const uint8_t tetrimino_spawn[TETRIMINO_COUNT] = { 2, 2, 0, 0, 0, 2, 1 };
 
 // @formatter:on
-
 
