7a4da353d1e76954df360667472b8492ec9234f9
primeroragde <primeroragde@gmail.com>
2025-01-20 14:00:10 -0500

    Completed splash screen function
    All is well done.

--- Core/Src/ui.c
+++ Core/Src/ui.c
@@ -38,106 +38,111 @@ void ui_init() {
 }
 
 void ui_splash_screen() {
-//    ssd1306_SetContrast(50); // to Dr. Butler - what does this do? i tried to change value but it seems to do nothing
-//    ssd1306_Fill(Black);
-//
-//    ssd1306_SetCursor(5, 8);
-//    ssd1306_WriteString("Imagine", Font_7x10, White);
-//
-//    ssd1306_SetCursor(35, 27);
-//    ssd1306_WriteString("RI ", Font_16x24, White);
-//
-//    ssd1306_SetCursor(59, 55); // x y
-//    ssd1306_WriteString("presents...", Font_6x8, White);
-//
-//    ssd1306_UpdateScreen();
-//    int bitmap_number = 0;
-//
-//    for (int i = 70; i <= 86; i += 3) { // Moves right
-//        ssd1306_WriteString("  ", Font_11x18, White);
-//        ssd1306_SetCursor(i, 0);
-//        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[0], 20, 13, White);
-//        ssd1306_UpdateScreen();
-//        osDelay(150);
-//    }
-//    bitmap_number++;
-//    for (int i = 86; i >= 60; i -= 3) { // Moves left
-//        ssd1306_WriteString("  ", Font_16x24, White);
-//        ssd1306_SetCursor(i, 0);
-//        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[bitmap_number], 13, 20, White);
-//        ssd1306_UpdateScreen();
-//        osDelay(150);
-//    }
-//
-//    for (int i = 60; i <= 66; i += 3) { // Moves right to center again
-//        ssd1306_WriteString("  ", Font_16x24, White);
-//        ssd1306_SetCursor(i, 0);
-//        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[bitmap_number], 13, 20, White);
-//        ssd1306_UpdateScreen();
-//        osDelay(150);
-//    }
-//
-//    bitmap_number++;
-//    for (int i = 0; i < 30; i += 3) { // Moves down
-//        if (i == 0) {
-//            ssd1306_WriteString("  ", Font_16x24, White);
-//        }
-//        ssd1306_WriteString("  ", Font_11x18, White);
-//        ssd1306_SetCursor(66, i);
-//        ssd1306_DrawBitmap(66, i, tetrimino_allArray[bitmap_number], 20, 13, White);
-//        ssd1306_UpdateScreen();
-//        osDelay(150);
-//    }
-//
-//    for (int i = 0; i < 8; i++) { // Light flashing
-//        if (i % 2 == 0) {
-//            ssd1306_Fill(White);
-//
-//            ssd1306_SetCursor(5, 8);
-//            ssd1306_WriteString("Imagine", Font_7x10, Black);
-//
-//            ssd1306_SetCursor(35, 27);
-//            ssd1306_WriteString("RI ", Font_16x24, Black);
-//
-//            ssd1306_DrawBitmap(66, 27, tetrimino_allArray[bitmap_number], 20, 13, Black);
-//
-//            ssd1306_SetCursor(59, 55); // x y
-//            ssd1306_WriteString("presents...", Font_6x8, Black);
-//
-//            ssd1306_UpdateScreen();
-//            osDelay(50);
-//        }
-//        ssd1306_Fill(Black);
-//
-//        ssd1306_SetCursor(5, 8);
-//        ssd1306_WriteString("Imagine", Font_7x10, White);
-//
-//        ssd1306_SetCursor(35, 27);
-//        ssd1306_WriteString("RI ", Font_16x24, White);
-//
-//        ssd1306_DrawBitmap(66, 27, tetrimino_allArray[bitmap_number], 20, 13, White);
-//
-//        ssd1306_SetCursor(59, 55); // x y
-//        ssd1306_WriteString("presents...", Font_6x8, White);
-//
-//        ssd1306_UpdateScreen();
-//        osDelay(50);
-//    }
+    ssd1306_SetContrast(50); // to Dr. Butler - what does this do? i tried to change value but it seems to do nothing
+    ssd1306_Fill(Black);
+
+    ssd1306_SetCursor(5, 8);
+    ssd1306_WriteString("Imagine", Font_7x10, White);
+
+    ssd1306_SetCursor(35, 27);
+    ssd1306_WriteString("RI ", Font_16x24, White);
+
+    ssd1306_SetCursor(59, 55); // x y
+    ssd1306_WriteString("presents...", Font_6x8, White);
+
+    ssd1306_UpdateScreen();
+    int bitmap_number = 0;
+
+    for (int i = 100; i >= 60; i -= 10) { // Moves left
+        ssd1306_SetCursor(i, 0);
+        ssd1306_WriteString("    ", Font_16x24, White);
+        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[bitmap_number], 20, 13, White);
+        ssd1306_UpdateScreen();
+        osDelay(350);
+    }
+
+    bitmap_number++;
+    for (int i = 60; i <= 90; i +=10) { // Moves right}
+    	ssd1306_SetCursor(i, 0);
+        ssd1306_WriteString("     ", Font_16x26, White);
+        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[bitmap_number], 13, 20, White);
+        ssd1306_UpdateScreen();
+		ssd1306_SetCursor((i-5), 0); // to clean up after displaying
+		ssd1306_WriteString("     ", Font_16x26, White);
+        osDelay(350);
+    }
+
+    bitmap_number++;
+
+    for (int i = 90; i >= 66; i -= 10) { // Moves right to center again
+        ssd1306_WriteString("  ", Font_16x24, White);
+        ssd1306_SetCursor(i, 0);
+        ssd1306_DrawBitmap(i, 0, tetrimino_allArray[bitmap_number], 20, 13, White);
+        ssd1306_UpdateScreen();
+        osDelay(350);
+    }
+
+    for (int i = 0; i < 30; i += 10) { // Moves down
+        if (i == 0) {
+            ssd1306_WriteString("  ", Font_16x24, White);
+        }
+        ssd1306_WriteString("  ", Font_11x18, White);
+        ssd1306_SetCursor(66, i);
+        ssd1306_DrawBitmap(66, i, tetrimino_allArray[bitmap_number], 20, 13, White);
+        ssd1306_UpdateScreen();
+        osDelay(350);
+    }
+
+    for (int i = 0; i < 8; i++) { // Light flashing
+        if (i % 2 == 0) {
+            ssd1306_Fill(White);
+
+            ssd1306_SetCursor(5, 8);
+            ssd1306_WriteString("Imagine", Font_7x10, Black);
+
+            ssd1306_SetCursor(35, 27);
+            ssd1306_WriteString("RI ", Font_16x24, Black);
+
+            ssd1306_DrawBitmap(66, 27, tetrimino_allArray[bitmap_number], 20, 13, Black);
+
+            ssd1306_SetCursor(59, 55); // x y
+            ssd1306_WriteString("presents...", Font_6x8, Black);
+
+            ssd1306_UpdateScreen();
+            osDelay(50);
+        }
+        ssd1306_Fill(Black);
+
+        ssd1306_SetCursor(5, 8);
+        ssd1306_WriteString("Imagine", Font_7x10, White);
+
+        ssd1306_SetCursor(35, 27);
+        ssd1306_WriteString("RI ", Font_16x24, White);
+
+        ssd1306_DrawBitmap(66, 27, tetrimino_allArray[bitmap_number], 20, 13, White);
+
+        ssd1306_SetCursor(59, 55); // x y
+        ssd1306_WriteString("presents...", Font_6x8, White);
+
+        ssd1306_UpdateScreen();
+        osDelay(50);
+    }
 
     ssd1306_Fill(Black);
     ssd1306_DrawBitmap(33, 11, tetrimino_allArray[4], 63, 16, White); // Tetris logo
-    ssd1306_DrawBitmap(24, 0, tetrimino_allArray[3], 75, 55, White); // T DOWN but bigger
+    ssd1306_DrawBitmap(24, 0, tetrimino_allArray[3], 75, 55, White); // T DOWN outline but bigger
     ssd1306_UpdateScreen();
-    ssd1306_SetCursor(30, 55); // x y
 
-    for(;;)
+    while(1)  // 'Press Start' blinking cycle
     {
+    	ssd1306_SetCursor(30, 55);
         ssd1306_WriteString("Press start", Font_6x8, White);
-        osDelay(100);
         ssd1306_UpdateScreen();
+        osDelay(600);
+        ssd1306_SetCursor(30, 55);
         ssd1306_WriteString("                ", Font_6x8, White); // erasing
-        ssd1306_WriteString("Press start", Font_6x8, Black);
         ssd1306_UpdateScreen();
+        osDelay(600);
     }
 
 }
