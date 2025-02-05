03bdafd4f80f4c3642aca9438484bb3d5b07a14f
dr. josh butler <josh.butler929@gmail.com>
2025-02-05 16:16:50 -0500

    add function to calculate time difference

--- Core/Inc/util.h
+++ Core/Inc/util.h
@@ -26,6 +26,7 @@
 
 uint8_t util_time_expired_delay(uint32_t start, uint32_t delay);
 uint8_t util_time_expired(uint32_t start, uint32_t end);
+uint32_t util_time_diff_us(uint32_t start, uint32_t end);
 void util_delay_us(uint32_t us);
 void util_to_binary32(uint32_t num, char *binary);
 void util_to_binary16(uint16_t num, char *binary);
