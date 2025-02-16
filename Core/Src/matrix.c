74fb8edae8a17f3a74e7f872cfae90ecedee3f2b
dr. josh butler <31372342+shuki25@users.noreply.github.com>
2025-02-16 14:17:09 -0500

    restore a block of code

--- Core/Src/matrix.c
+++ Core/Src/matrix.c
@@ -80,6 +80,17 @@ matrix_status_t matrix_add_tetrimino(matrix_t *matrix, tetrimino_t *tetrimino) {
     shape_offset = tetrimino->shape_offset;
     row_index = tetrimino->y + TETRIMINO_CENTER_Y;
 
+    // Check if tetrimino has reached beyond the bottom of the matrix
+    for (int i = 0; i < TETRIMINO_BLOCK_SIZE; i++) {
+        // When row_index becomes "negative", it rolls over to 255, it will be greater than PLAYING_FIELD
+        if (tetrimino_shape[shape_offset + i] && row_index >= PLAYING_FIELD_HEIGHT) {
+            return MATRIX_REACHED_BOTTOM;
+        }
+        row_index--;
+    }
+
+    // Superimpose tetrimino on playfield
+    row_index = tetrimino->y + TETRIMINO_CENTER_Y;
     for (int i = 0; i < TETRIMINO_BLOCK_SIZE; i++) {
         if (row_index >= PLAYING_FIELD_HEIGHT) { // Check if row is within visible bounds
             row_index--;
@@ -122,7 +133,7 @@ matrix_status_t matrix_add_tetrimino(matrix_t *matrix, tetrimino_t *tetrimino) {
 
     return MATRIX_REFRESH;
 
-	// TODO: Update palette bitboards
+    // TODO: Update palette bitboards
 }
 
 /**
@@ -131,7 +142,7 @@ matrix_status_t matrix_add_tetrimino(matrix_t *matrix, tetrimino_t *tetrimino) {
  * @retval None
  */
 void matrix_flatten(void) {
-	// TODO: Flatten bitboards (tetrimino, fallen blocks, palette) into final matrix
+    // TODO: Flatten bitboards (tetrimino, fallen blocks, palette) into final matrix
 }
 
 /**
@@ -140,7 +151,7 @@ void matrix_flatten(void) {
  * @retval None
  */
 void matrix_clear(void) {
-	// TODO: Clear fallen blocks from matrix
+    // TODO: Clear fallen blocks from matrix
 }
 
 /**
@@ -149,22 +160,21 @@ void matrix_clear(void) {
  * @retval True if collision, false otherwise
  */
 matrix_status_t matrix_check_collision(matrix_t *matrix, tetrimino_t *tetrimino) {
-	// TODO: Check for collision between tetrimino, boundaries, and fallen blocks
-        uint8_t row_index;
-        uint32_t working_playfield = 0;
-        uint32_t working_stack = 0;
+    // TODO: Check for collision between tetrimino, boundaries, and fallen blocks
+    uint8_t row_index;
+    uint32_t working_playfield = 0;
+    uint32_t working_stack = 0;
 
-
-        for(row_index = 0; row_index < PLAYING_FIELD_HEIGHT; row_index++){
-            working_stack = matrix->stack[row_index];
-            working_playfield = matrix->playfield[row_index];
-            if(working_stack & working_playfield){
-                return MATRIX_BLOCK_COLLISION;
-            }
+    for (row_index = 0; row_index < PLAYING_FIELD_HEIGHT; row_index++) {
+        working_stack = matrix->stack[row_index];
+        working_playfield = matrix->playfield[row_index];
+        if (working_stack & working_playfield) {
+            return MATRIX_BLOCK_COLLISION;
+        }
 
         return MATRIX_OK;
 
-}
+    }
 }
 
 /**
@@ -173,7 +183,7 @@ matrix_status_t matrix_check_collision(matrix_t *matrix, tetrimino_t *tetrimino)
  * @retval True if line clear, false otherwise
  */
 void matrix_check_line_clear(matrix_t *matrix) {
-	// TODO: Check for line clear (full rows)
+    // TODO: Check for line clear (full rows)
 }
 
 /**
@@ -182,9 +192,9 @@ void matrix_check_line_clear(matrix_t *matrix) {
  * @retval Number of rows cleared
  */
 void matrix_line_clear(void) {
-	// TODO: Clear full rows
+    // TODO: Clear full rows
 
-	// TODO: Reposition fallen blocks
+    // TODO: Reposition fallen blocks
 }
 
 void matrix_debug_print(matrix_t *matrix) {
